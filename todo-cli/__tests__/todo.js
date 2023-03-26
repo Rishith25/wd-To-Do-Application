10eacfee4b2f565e4405ef37014c80a500791ae8
rishith <rishith@DESKTOP-HFE4ENB>
2023-03-26 17:16:46 +0530

    Add pre-commit hook to run tests

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-// const { beforeAll } = require('jest-circus');
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const {
+  all,
+  markAsComplete,
+  add,
+  overdue,
+  dueToday,
+  dueLater,
+  toDisplayableList,
+} = todoList();
 
 // eslint-disable-next-line no-undef
 describe("Todolist Test Suite", () => {
@@ -12,22 +19,49 @@ describe("Todolist Test Suite", () => {
       title: "Test todo",
       completed: false,
       dueDate: new Date().toISOString().split("T")[0],
-      // dueDate : new Date().toLocaleDateString("en-CA")
     });
   });
+
   test("Should add new todo", () => {
     const todoItemsCount = all.length;
     add({
-      title: "Test todo",
+      title: "Test todo-1",
       completed: false,
-      dueDate: new Date().toISOString().split("T")[0],
-      // dueDate : new Date().toLocaleDateString("en-CA")
+      dueDate: "2023-01-01",
     });
     expect(all.length).toBe(todoItemsCount + 1);
   });
+
   test("Should mark a todo as complete", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("Should retrieve overdue items", () => {
+    const overdueItems = all.filter(
+      (item) =>
+        new Date(item.dueDate).toISOString().split("T")[0] <
+        new Date().toISOString().split("T")[0]
+    );
+    expect(overdue()).toEqual(overdueItems);
+  });
+
+  test("Should retrieve Due Today items", () => {
+    const dueTodayItems = all.filter(
+      (item) =>
+        new Date(item.dueDate).toISOString().split("T")[0] ===
+        new Date().toISOString().split("T")[0]
+    );
+    expect(dueToday()).toEqual(dueTodayItems);
+  });
+
+  test("Should retrieve Due Later items", () => {
+    const dueLaterItems = all.filter(
+      (item) =>
+        new Date(item.dueDate).toISOString().split("T")[0] >
+        new Date().toISOString().split("T")[0]
+    );
+    expect(dueLater()).toEqual(dueLaterItems);
   });
 });
